eaac1a5db09bbbcac53d59df433e912f0ba0581e
vishnu vardhan <vishnuvardhan@vishnus-MacBook-Pro.local>
2022-12-03 13:41:58 +0530

    test transaction working

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { ethers } from "https://cdn.ethers.io/lib/ethers-5.2.esm.min.js";
 
 let provider
+let signer
 
 function validateMetamaskConnection() {
     let { ethereum } = window
@@ -16,19 +17,21 @@ async function getSignerAddress() {
     try {
         await ethereum.request({ method: "eth_requestAccounts" })
         provider = new ethers.providers.Web3Provider(ethereum);
-        return await provider.getSigner().getAddress();
+        signer = await provider.getSigner()
+        return signer.getAddress();
     } catch (err) {
         console.log("failed to get signer address");
     }
 }
 
-function transferEth(fromAddress, toAddress, value) {
+async function transferEth(fromAddress, toAddress, value) {
     if (!provider) return;
-    provider.sendTransaction({
+    const params = [{
         from: fromAddress,
         to: toAddress,
-        value: String(value * Math.pow(10, 18))
-    }).then(res => {
+        value: ethers.utils.parseUnits(value, 'ether').toHexString()
+    }];
+    provider.send('eth_sendTransaction', params).then(res => {
         console.log(res)
     }).catch(err => {
         console.log(err)
@@ -38,7 +41,7 @@ function transferEth(fromAddress, toAddress, value) {
 async function main() {
     validateMetamaskConnection();
     let signer = await getSignerAddress();
-    transferEth(signer, "0x666bC642c526DaD7211de4df1DEd418596F693AC", "0.001")
+    await transferEth(signer, "0x666bC642c526DaD7211de4df1DEd418596F693AC", "0.001")
 }
 
 main().catch(err => {
