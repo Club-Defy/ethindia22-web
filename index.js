4f9b4e0a94bf63e4725043e9772f2de08f52d68d
vishnu vardhan <vishnuvardhan@vishnus-MacBook-Pro.local>
2022-12-03 13:03:29 +0530

    test transfers payload

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import { ethers } from "https://cdn.ethers.io/lib/ethers-5.2.esm.min.js";
 
+let provider
+
 function validateMetamaskConnection() {
     let { ethereum } = window
 
@@ -13,17 +15,30 @@ async function getSignerAddress() {
 
     try {
         await ethereum.request({ method: "eth_requestAccounts" })
-        let provider = new ethers.providers.Web3Provider(ethereum);
+        provider = new ethers.providers.Web3Provider(ethereum);
         return await provider.getSigner().getAddress();
     } catch (err) {
         console.log("failed to get signer address");
     }
 }
 
+function transferEth(fromAddress, toAddress, value) {
+    if (!provider) return;
+    provider.sendTransaction({
+        from: fromAddress,
+        to: toAddress,
+        value: String(value * Math.pow(10, 18))
+    }).then(res => {
+        console.log(res)
+    }).catch(err => {
+        console.log(err)
+    })
+}
+
 async function main() {
     validateMetamaskConnection();
     let signer = await getSignerAddress();
-    console.log(signer)
+    transferEth(signer, "0x666bC642c526DaD7211de4df1DEd418596F693AC", "0.001")
 }
 
 main().catch(err => {
